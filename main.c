7a32180dc4b61764c3d1d76d2e079a2d6cc681fe
Mehmet Emin POLAT <mepolat17@gmail.com>
2024-12-08 22:29:03 +0300

    v2

--- main.c
+++ main.c
@@ -1,10 +1,27 @@
-\\writed by oplta⚡
+//written by oplta⚡
 #include <stdio.h>
 #include <stdlib.h>
-#include <string>
+#include <string.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    char isim[50];
+    char cinselTercih[50];
+    int yas;
 
+    printf("Adınızı Giriniz ");
+    scanf("%s", isim);
+
+
+    printf("Gay misin? ");
+    scanf("%s", cinselTercih);
+
+    printf("Yaşınızı Giriniz ");
+    scanf("%d", &yas);
+
+
+
+    printf("O zaman senin adın %s ve yaşın %d. \nCinsel yönelimin umrumda değil! Sadece Sormak istedim lol.", isim, yas );
+
+    return 0;
 }
